683b0ae146cc8d33c58ae309db8063f150104b31
Kushal Khattri <kushalk62@gmail.com>
2021-12-03 22:04:48 -0800

    added api and header

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
 var apikey = "82ccbfcc5db7584829b51304b0040048"
 var btnEl = $("#searchbtn");
-function searchcity(){
-var search = $("#search");
-var searchval= search.val();
-console.log(searchval);
-var link = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid="+apikey;
-var links ="https://api.openweathermap.org/data/2.5/weather?q="+searchval+"&appid="+apikey;
-fetch(links).then(function(res){
-    return res.json()
-}) .then(function(data){
-    console.log(data)
-})
+function searchcity() {
+    var search = $("#search");
+    var searchval = search.val();
+    console.log(searchval);
+
+    var links = "https://api.openweathermap.org/data/2.5/weather?q=" + searchval + "&appid=" + apikey;
+    fetch(links).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        console.log(data);
+        var link = "https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=" + apikey;
+        fetch(link).then(function(res){
+            return res.json();
+        }).then(function(info){
+            console.log(info);
+
+        })
+    })
 }
 btnEl.click(searchcity);
